e052c0fd897ec8d33f050e07cac47512aa103796
tiansivive <vilaverde@exabel.com>
2023-07-22 14:43:22 +0200

    First commit

---
+++ src/grid/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div >
+      <section>
+        <
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+       
+      </section>
+    </div>
+  );
+}
+
+export default App;
